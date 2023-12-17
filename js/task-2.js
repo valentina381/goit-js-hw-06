c2d39a6a8fea0957fe2c2ca31283313b42ad1b14
valentina381 <77145765+valentina381@users.noreply.github.com>
2023-12-17 14:37:29 +0100

    HW js 6

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+'use strict';
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+    // this.#items = [...this.#items, newItem];
+  }
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((item) => item !== itemToRemove);
+    // const index = this.#items.findIndex((item) => item === itemToRemove);
+    // this.#items.splice(index, 1);
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
